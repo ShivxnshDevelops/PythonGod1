2154b892bcc9333a9bc228c4d0f79b2ca009eeb4
Samarth <samarplayz1337@gmail.com>
2024-10-30 19:46:54 +0530

    added a better menu and.. ,fixed minor spelling mistakess

--- Case.py
+++ Case.py
@@ -2,7 +2,7 @@ Wel = "Welcome To The Program , The Author Of The Program Is ShivxnshDevelops"
 print(Wel.center(50))
 
 x = input("\nEnter The String Of which You Wanna Change The Something :")
-print("""Enter 1 If You Want To Change it In Upper Case \nEnter 2 If You Want To Change it In Lower Case \nEnter 3 If You Want To Remve Any Special Characters \nEnter 4 if You Want To Replace The String With Something \nEnter 5 If You Want To Use The Captalize Case""")
+print("""1: If You Want To Change it In Upper Case \n2: If You Want To Change it In Lower Case \n3: If You Want To Remove Any Special Characters \n4: if You Want To Replace The String With Something else.\n5: If You Want To Use The Captalize Case""")
 
 
 K = input("Enter The Value According to The Instructions Given Above :")
@@ -20,7 +20,6 @@ elif K=="4" :
     print(x.replace( a , n))
 elif K=="5" :
     print(x.capitalize())
-
 else :
     print("The Value You Entered Is InVailid \n Kindly Read The Instructions Carefully <3")
 
