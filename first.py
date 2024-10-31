a559e78022705a16b481ba2a808c9c5e4d9021c1
ShivxnshDevelops <shivxnshdevelops@gmail.com>
2024-10-31 06:57:22 +0530

    Development File Of Time2.py

--- first.py
+++ first.py
@@ -1,3 +1,38 @@
-print("wtf Happend to you")
-nm = "Harry"
-print(nm[-4:-2])
+import time
+import datetime as dt
+import pytz
+
+
+name = input("Enter Your Name :")
+Recenttime = time.strftime('%H :%M')
+recenttime = time.strftime("%H")
+
+
+
+print("India ' s Code is Ind \n America ' s Code is USA")
+
+z = pytz.timezone("Europe/France")
+
+x = str(input("Enter The Country Code :"))
+
+if x=="Ind" :
+    if recenttime=="> 12" or "< 4" :
+        print("Good Morning" , name ,"The Time Right Now Is " , Recenttime)
+    elif recenttime=="12" :
+        print("Good Afternoon" , name , "The Time Right Now Is" , Recenttime)
+    elif recenttime=="< 12" or "> 18" :
+        print("Good Evening" , name , "The Time Right Now Is" , Recenttime)
+    else : 
+        print("Good Night" , name , "The Time Right Now is " , Recenttime) 
+
+
+
+elif x=="USA":
+        if z=="> 12" or "< 4" :
+            print("Good Morningh" , name ,"The Time Right Now Is " , Recenttime)
+        elif z=="12" :
+            print("Good Afternoon" , name , "The Time Right Now Is" , Recenttime)
+        elif z=="< 12" or "> 18" :
+            print("Good Evening" , name , "The Time Right Now Is" , Recenttime)
+        else : 
+            print("Good Night" , name , "The Time Right Now is " , Recenttime)
