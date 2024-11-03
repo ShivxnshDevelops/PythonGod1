57e3e7e55e766ba25b8c8826fe9cff622c6d5b47
ShivxnshDevelops <shivxnshdevelops@gmail.com>
2024-11-03 19:11:51 +0530

    Day 5 Of Mastering Python in 30 Days

---
+++ loopz.py
@@ -0,0 +1,13 @@
+# For Loop Programing
+name = "SK"
+
+for i in name :
+    print(i)
+    if (i =="K"):
+        print("This Is Something Special")
+for characters in name :
+    print(characters)
+for k in range(20 , 28 , 2):
+    print(k)
+
+
