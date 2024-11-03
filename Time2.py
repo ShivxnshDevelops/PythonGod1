91adb5dbed0b6fb888058c7121c9b21746cb8a62
ShivxnshDevelops <shivxnshdevelops@gmail.com>
2024-11-03 18:28:11 +0530

    Editing The Code

--- Time2.py
+++ Time2.py
@@ -9,13 +9,13 @@ recenttime = time.strftime("%H")
 
 
 
-if recenttime=="> 12" or "< 4" :
+if recenttime=="<=4" or "<12" :
     print("Good Morningh" , name ,"The Time Right Now Is " , Recenttime)
-elif recenttime=="12" :
+elif recenttime==">=12" or "< 16" :
      print("Good Afternoon" , name , "The Time Right Now Is" , Recenttime)
-elif recenttime=="< 12" or "> 18" :
+elif recenttime==">=16" or "< 20" :
     print("Good Evening" , name , "The Time Right Now Is" , Recenttime)
-elif recenttime=="< 18" or "> 4" : 
+elif recenttime==">= 20" or "> 4" : 
     print("Good Night" , name , "The Time Right Now is " , Recenttime) 
 else :
     input("Enter 1 To Retry The Program")
