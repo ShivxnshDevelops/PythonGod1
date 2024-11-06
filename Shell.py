ce48179707ca4268070670838f7cec68604f3f32
ShivxnshDevelops <shivxnshdevelops@gmail.com>
2024-11-06 21:31:23 +0530

    Added Auto Run Instead Of Error In The Program

--- Shell.py
+++ Shell.py
@@ -1,6 +1,7 @@
 # Match Cases File 
 x = int(input("Enter The Shell No.Of The Atomic Structure :"))
 
+
 match x:
     # If Case Is 1 Then
     case 1:
@@ -15,8 +16,30 @@ match x:
     case 4:
         print("The No Of Atoms Can Be Stored In The Fourth Shell Are" ,  2*x**2)
     case _:
-        print("Invailid Shell No. | ErrOr 404 | Command Not Found")
+            while(x>4):
+                 x = int(input("Please ReEnter The Shell No.Of The Atomic Structure :"))
+                 while(x=="1" , "2" , "3" , "4"):
+                      match x:
+                     # If Case Is 1 Then
+                         case 1:
+                            print("The No Of Atoms Can Be Stored In The First Shell Are" , 2*x**2)
+                            x = x - 1
 
+                     # if Case Is 2 Then
+                         case 2:
+                             print("The No Of Atoms Can Be Stored In The Second Shell Are" ,  2*x**2)
+                             x = x - 2
+
+                     # if Case Is 3 Then
+                         case 3:
+                             print("The No Of Atoms Can Be Stored In The Third Shell Are" ,  2*x**2)
+                             x = x - 3
+                    # if Case Is 3 Then
+                         case 4:
+                             print("The No Of Atoms Can Be Stored In The Fourth Shell Are" ,  2*x**2)
+                             x = x - 4
+
+     
     
        
 
