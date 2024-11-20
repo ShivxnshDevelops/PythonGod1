5ded36f6c98da529028e3e95ee53cf0c74b54528
ShivxnshDevelops <shivxnshdevelops@gmail.com>
2024-11-20 05:45:35 +0530

    Joule Converter From KiloWattHour

---
+++ power.py
@@ -0,0 +1,8 @@
+def joulecnverter(a):
+
+  print("The Value Of KiloWattHour In Joules Is" , a*(3.6 *  10**6))
+
+a =int( input("Enter The Value Of KilowattHour :"))
+joulecnverter(a)
+
+# Relation between Kinetic Enery & Momntum is [ K = 1/2 * p**2 ] / 2m 
