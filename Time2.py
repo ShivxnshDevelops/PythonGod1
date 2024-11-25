4e7a28f6763be671e389ecd44c5985994fef2c15
ShivxnshDevelops <shivxnshdevelops@gmail.com>
2024-11-25 20:00:50 +0530

    Code Dump python Greeter

--- Time2.py
+++ Time2.py
@@ -13,8 +13,5 @@ if(x>=0 and x<12):
     print("Good Morning" , name , "the Time Right Now is" , Recenttime)
 elif(x>=12 and x<18):
     print("Good Afternoon" , name , "the Time Right Now is" , Recenttime)
-
-elif(x>=18 and x<0):
-    print("Good Evening" , name , "the Time Right Now is" , Recenttime)
 else:
     print("Good Night" , name , "the Time Right Now is" , Recenttime)
