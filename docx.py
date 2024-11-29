496bcff49dde5465fb105313a2711c37fe710c1f
ShivxnshDevelops <shivxnshdevelops@gmail.com>
2024-11-29 06:45:03 +0530

    Doc-Strs And Factorial FUnction | Code Dump

--- docx.py
+++ docx.py
@@ -1,8 +1,11 @@
 print("This File Is For Learning DocStrings In Python \nAuthor Of The Progrm - Shiv4nsh")
 def factorial(a):
+    '''This Function Used For The Factorial Of The Intger/Number in Python'''
     if(a==0 or a==1):
         return 1
     else :
-        a * factorial(a-1)
-a = int(input("Enter The Value For The Factorial : "))
-factorial(a)
+        return a * factorial(a-1)
+    
+a = int(input("Enter The Value For The Factorial :"))
+print(factorial(a))
+print(factorial.__doc__)
