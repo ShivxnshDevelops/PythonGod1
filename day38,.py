f982471be8d758425c18d075b5440941b1a65bcf
ShivxnshDevelops <shivxnshdevelops@gmail.com>
2024-12-01 19:19:56 +0530

    Else In Loops | Code Dump

---
+++ day38,.py
@@ -0,0 +1,27 @@
+print("Today I Am Goin To Learn The For Loops In Else")
+x = 0
+# while x<9:
+#     x = x + 1
+#     print(f"The Value Of  X  Rn Is {x}")
+# else :
+#     print("The Loop Is Succesfully Terminated")
+# while x<9:
+#     x = x + 1
+#     print(f"The Value Of  X  Rn Is {x}")
+#     if x==7:
+#         print("The Loop Is Broken In Between Executeing The Value")
+#         break
+# else :
+#     print("The Loop Is Succesfully Terminated")
+# for x in range(9):
+#     print(f"The Value of  x rn is {x}")
+#     if x==7:
+#         print("The Loop Is Broken Down In Between Executing The Code")
+#         break
+# else :
+#     print("The Loop Is Succesfully Terminated")
+
+for x in range(9):
+    print(f"The Value of x rn is {x}")
+else :
+    print("The Loop IS Succesfully Terminated")
